0777c52d116608532b6ffb03244e11f86f66294f
Jerry Huckins <jerryhuckins@gmail.com>
2020-12-12 23:49:57 -0600

    added a version return to root endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
-export const sum = (a: number, b: number) => {
-  if ("development" === process.env.NODE_ENV) {
-    console.log("boop")
-  }
-  return a + b
-}
+import express from "express"
+
+const app = express()
+const port = 3000
+
+app.get("/", (req: any, res: any) => {
+  console.log(req)
+  res.send(process.env.npm_package_version)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+console.log(app)
+
+export default app
+
+// export const sum = (a: number, b: number) => {
+//   if ("development" === process.env.NODE_ENV) {
+//     console.log("boop")
+//   }
+//   return a + b
+// }
