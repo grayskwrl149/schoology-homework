09b2e7b148e3a2a02223045686e5b02b9fa689dd
Jerry Huckins <jerryhuckins@gmail.com>
2020-12-14 01:18:37 -0600

    added swagger docs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express from "express"
+import swaggerUi from "swagger-ui-express"
+import openApiDocumentation from "./openApiDocumentation"
 
 const app = express()
 const port = 3000
 
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(openApiDocumentation))
+
 app.get("/", (req: any, res: any) => {
   console.log(req)
-  res.send(process.env.npm_package_version)
+  res.send({
+    name: process.env.npm_package_name,
+    packageVersion: process.env.npm_package_version,
+  })
 })
 
 app.listen(port, () => {
---
+++ src/openApiDocumentation.ts
@@ -0,0 +1,46 @@
+const openApiDocumentation = {
+  openapi: "3.0.1",
+  info: {
+    title: "Starter API",
+    description: "Starter package for an API",
+    version: "1.0.0",
+  },
+  servers: [
+    {
+      url: "http://localhost:3000",
+      description: "local",
+    },
+  ],
+  paths: {
+    "/": {
+      get: {
+        summary: "returns package info",
+        responses: {
+          "200": {
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    name: {
+                      type: "string",
+                    },
+                    version: {
+                      type: "string",
+                    },
+                  },
+                  example: {
+                    name: "api-starter",
+                    version: "1.0.0",
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
+
+export default openApiDocumentation
