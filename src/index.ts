ae63c8ffbb6f2259a3eb6681cdb456895ffa7cf8
Jerry <jerryhuckins@gmail.com>
2020-12-18 00:59:39 -0600

    now using express.Router

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,21 @@ console.log(exampleVar)
 
 const app = express()
 const port = 3000
+const router = express.Router()
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(openApiDocumentation))
+router.use("/api_docs", swaggerUi.serve)
+router.get("/api_docs", swaggerUi.setup(openApiDocumentation))
 
-app.get("/", (req: any, res: any) => {
+router.get("/ping", (req: any, res: any, next: any) => {
   console.log(typeof req) //TODO: this stops breaking error caused by 'req' not being used
   res.send({
     name: process.env.npm_package_name,
-    packageVersion: process.env.npm_package_version,
+    version: process.env.npm_package_version,
   })
+  next()
 })
+
+app.use(router)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
